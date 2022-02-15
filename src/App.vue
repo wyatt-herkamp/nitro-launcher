bd700a35ffd1928e409e08ab8f9b2cc782278ca2
Wyatt J Herkamp <wherkamp@kingtux.me>
2022-02-14 23:26:00 -0500

    Outlining a frontend

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,80 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld />
+  <div class="flex flex-row">
+    <div class="w-full">
+      <h1>Hey</h1>
+    </div>
+    <div class="min-h-screen flex">
+      <div class="flex flex-col w-56 bg-slate-800 rounded-l-3xl">
+        <div class="flex-col min-h-fit">
+          <canvas id="skin_container"></canvas>
+          <select
+            aria-label="select box"
+            onchange="switchMode(event)"
+            class="
+              w-full
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500
+              focus:outline-none
+              pl-10
+              py-2
+              appearance-none
+              flex
+              items-center
+              h-12
+              border border-gray-700
+              dark:border-gray-50
+              rounded-2xl
+              text-sm
+              leading-5
+              dark:bg-gray-900 dark:text-gray-50
+            "
+          >
+            <option selected="" value="auto">Welcome, KingTux</option>
+          </select>
+        </div>
+
+        <ul class="flex flex-col min-h-fit">
+          <li
+            class="
+              flex flex-row
+              items-center
+              h-12
+              transform
+              ease-in
+              duration-200
+              text-slate-50
+              hover:text-slate-100
+            "
+          >
+            <span class="font-bold text-sm">News</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { SkinViewer } from "skinview3d";
+export default defineComponent({
+  setup() {},
+  mounted() {
+    let value = document.getElementById("skin_container");
+    console.log(value);
+    const skinViewer = new SkinViewer({
+      canvas: value,
+      width: 200,
+      height: 200,
+
+      skin: "https://crafatar.com/skins/d087006b-d72c-4cdf-924d-6f903704d05c",
+    });
+    // Set the background to a panoramic image
+    skinViewer.loadPanorama(
+      "https://github.com/bs-community/skinview3d/blob/master/examples/img/panorama.png?raw=true"
+    );
+  },
+});
+</script>
